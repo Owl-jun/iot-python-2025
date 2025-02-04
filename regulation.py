046598db2739680f4baf05b30824a8f6c9477af1
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-02-04 21:30:01 +0900

    2일차 정규식 연습

--- regulation.py
+++ regulation.py
@@ -1,4 +1,7 @@
-import re
+import re 
 
-_input = "Hello, World!"
-p = re.match()
+problem = "010-9501-1650"
+p = re.compile("(\d{3})[-](\d{4})[-]\d{4}")
+print(p.sub("\g<1>-\g<2>-****",problem))
+
+

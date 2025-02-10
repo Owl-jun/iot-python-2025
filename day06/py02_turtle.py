2fbb1b2d768ea63683b917ae4b9cb0b130c9ffae
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2025-02-10 12:36:27 +0900

    6일차 오전커밋

---
+++ day06/py02_turtle.py
@@ -0,0 +1,22 @@
+# py02_turtle.py
+# 터틀 라이브러리는 주피터노트북에서 실행 불가
+import turtle as t
+
+t.home()
+
+t.shape('turtle')
+t.bgcolor("skyblue")
+
+t.forward(150)
+t.right(45)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(45)
+t.forward(150)
+t.right(45)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+t.done()
